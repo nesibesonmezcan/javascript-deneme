26311c023f0e7664fb77520c8120e1e27fe6b432
Nesibe Sönmezcan <nsonmezcan@gmail.com>
2023-05-08 23:47:44 +0300

    v08,05

--- day-04/js/script-19.js
+++ day-04/js/script-19.js
@@ -1,6 +1,8 @@
 function setMode(){
 const isDark=document.body.classList.contains("dark")
-console.log(isDark)
+if(isDark){
+document.body.classList.remove(("dar"))    
+}
 
 /*     document.body.classList.toggle('dark') */
 }
