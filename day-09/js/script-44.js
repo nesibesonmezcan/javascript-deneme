50745fae55483673cb1014f292fb784d99c89155
Nesibe Sönmezcan <nsonmezcan@gmail.com>
2023-05-13 23:30:44 +0300

    v13,05

--- day-09/js/script-44.js
+++ day-09/js/script-44.js
@@ -2,11 +2,14 @@ const txtNum = document.querySelector("#txtNum");
 const btnStart = document.querySelector("#btnStart");
 const btnGuess = document.querySelector("#btnGuess");
 const txtResult = document.querySelector("#txtResult");
+
 let randomNumber = 0;
 const minRandomNumber = 20;
 const maxRandomNumber = 80;
 const totalShot = 10;
+
 txtNum.setAttribute("placeholder", `Type number between ${minRandomNumber}-${maxRandomNumber}`)
+
 const start = () => {
     randomNumber = getRandomNumber();
     txtNum.removeAttribute("disabled");
@@ -16,17 +19,21 @@ const start = () => {
     btnStart.innerHTML = "Reset game";
     txtResult.innerHTML = "";
 }   
+
 const reset = () => {
     btnGuess.classList.add("d-none");
     txtNum.setAttribute("disabled","true");
     btnStart.innerHTML = "Start game";
 }
+
 const guess = () => {
     const guessNumber = Number(txtNum.value);
+
     if(!guessNumber || isNaN(guessNumber) || guessNumber>maxRandomNumber || guessNumber<minRandomNumber){
         txtResult.innerHTML = `Please type a number between ${minRandomNumber}-${maxRandomNumber}`;
         return;
     }
+
     if(guessNumber === randomNumber){
         txtResult.innerHTML = "🏆🏆🏆 You win! 🏆🏆🏆";
         txtResult.classList.replace("text-danger", "text-success");
@@ -38,14 +45,26 @@ const guess = () => {
     else{
         txtResult.innerHTML = `${guessNumber} is too low! Raise the number! 👍`;
     }
+
     txtNum.focus();
     txtNum.value="";
   
 }
+
+totalShot--;
+txtResult.innerHTML +="kalan hak sayısı
+"
+if(totalShot==0){
+    btnGuess.innerHTML=true;
+    btnStart.innerHTML=false;
+
+}
 const getRandomNumber = () => {
     return Math.floor(Math.random() * (maxRandomNumber - minRandomNumber + 1) ) + minRandomNumber;
+
     /* 
         Math.floor(Math.random() * (max - min + 1) ) + min;
+
         5-60 arasında (5 ve 60 dahil) sayı tutmak istiyorsak formül:
         Math.floor(Math.random() * (max - min + 1) ) + min;
         Math.floor(Math.random() * (60 - 5 + 1) ) + 5;
